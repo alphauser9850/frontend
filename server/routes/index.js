dba6d93998dd84a0d4698fa2b1dbb26113d9319d
root <aniket.gadkari@berkut.cloud>
2025-09-29 03:57:08 -0700

    local SSR

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import hubspotRouter from "./hubspotRoute.js";
-import paymentRouter from "./paymentRoute.js";
-const router = express.Router();
-
-router.use("/hubspot", hubspotRouter);
-router.use("/payment", paymentRouter);
-
-export default router;
