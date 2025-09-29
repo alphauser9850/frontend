ba6c88274ad23b1a26f2a1ff386ccdc5a6fd49ec
root <aniket.gadkari@berkut.cloud>
2025-09-29 03:57:08 -0700

    local SSR

--- start-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Navigate to the project directory
-cd "/var/www/berkut-cloud"
-
-# Serve the built files on the specified port
-echo "Starting server on port 3000..."
-/usr/bin/serve -s dist -l 3000
